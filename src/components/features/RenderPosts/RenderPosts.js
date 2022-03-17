d277799b56c0e200bc9922003f34f1a88feccd4b
Mariusz Banaś <mb0986@wp.pl>
2022-03-17 20:17:31 +0100

    Add content to main page

---
+++ src/components/features/RenderPosts/RenderPosts.js
@@ -0,0 +1,37 @@
+import { Row, Col, Button} from 'react-bootstrap';
+import { getAllPosts } from '../../../redux/postRedux';
+import { useSelector} from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const RenderPosts = () => {
+
+    const posts = useSelector(getAllPosts);
+
+    return (
+      <div>
+        <Row>
+          <Col><h1>All posts</h1></Col>
+          <Col className="text-end">
+            <Link to="/post/add">
+              <Button variant="outline-info">Add post</Button>
+            </Link>    
+          </Col>
+        </Row>
+        <Row className=" my-4 d-flex">
+          {posts.map(post => 
+            <Col className="border p-3 m-2 col-12 col-lg-3  flex-grow-1" 
+              key={post.id}>
+              <h4>{post.title}</h4>
+              <p className="m-0"><span className="fw-bold">Author:</span> {post.author}</p>
+              <p><span className="fw-bold">Published:</span> {post.publishedDate}</p>
+              <p>{post.shortDescription}</p>
+              <Link to={`/post/${post.id}`}>
+                <Button variant="primary">Read more</Button>
+              </Link> 
+            </Col>)}
+        </Row>
+      </div>
+    );
+};
+
+export default RenderPosts;
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,7 +1,9 @@
+import RenderPosts from "../../features/RenderPosts/RenderPosts";
+
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <RenderPosts />
         </div>
     );
 };
